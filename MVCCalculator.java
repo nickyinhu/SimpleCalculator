a5023870a581767397eeddc1ab73e228fceae2ac
nickyinhu <nickhodge8@Hotmail.com>
2014-09-14 20:28:37 -0500

    Create MVCCalculator.java

---
+++ MVCCalculator.java
@@ -0,0 +1,9 @@
+public class MVCCalculator{
+	public static void main (String[] args){
+		
+		CalculatorModel model = new CalculatorModel();
+		CalculatorView view = new CalculatorView();
+		CalculatorController controller = new CalculatorController(model, view);
+		view.setVisible(true);
+	}
+}
